f232117bb567db67695cc43a5de18b4a3dbf159a
levichen <levichen@nine-yi.com>
2015-10-27 22:08:28 +0800

    第二、三、四寫好後，相同邏輯的第五個測試: 一次買了整套，一二三四五集各買了一本，價格應為 100 * 5 * 0.75 = 375 也完成了

--- day2/test/PotterShoppingCartTest.js
+++ day2/test/PotterShoppingCartTest.js
@@ -39,6 +39,26 @@ describe('Potter Shopping Cart Tests', () => {
     actual.should.equal(expected);
   });
 
+  it('一二三集各買了一本，價格應為100 * 3 * 0.9 = 270', () => {
+    // arrange 
+    var books = [
+      new Book(1, 100),
+      new Book(2, 100),
+      new Book(3, 100)
+    ];
+
+    let target = new PotterShoppingCart();
+    target.setBooks(books);
+
+    let expected = 270;
+
+    //act
+    let actual = target.getPrice();
+
+    //assert
+    actual.should.equal(expected);
+  });
+
   it('一二三四集各買了一本，價格應為 100 * 4 * 0.8 = 320', () => {
     // arrange 
     var books = [
@@ -60,23 +80,27 @@ describe('Potter Shopping Cart Tests', () => {
     actual.should.equal(expected);
   });
 
-  it('一二三集各買了一本，價格應為100 * 3 * 0.9 = 270', () => {
+
+  it('一次買了整套，一二三四五集各買了一本，價格應為 100 * 5 * 0.75 = 375', () => {
     // arrange 
     var books = [
       new Book(1, 100),
       new Book(2, 100),
-      new Book(3, 100)
+      new Book(3, 100),
+      new Book(4, 100),
+      new Book(5, 100),
     ];
 
     let target = new PotterShoppingCart();
     target.setBooks(books);
 
-    let expected = 270;
+    let expected = 375;
 
     //act
     let actual = target.getPrice();
 
     //assert
     actual.should.equal(expected);
+
   });
 });
