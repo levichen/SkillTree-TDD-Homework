de924b14b59595841f6e3794a2c480530871ff7f
levichen <levichen@nine-yi.com>
2015-10-27 21:39:30 +0800

    Day2 homework red light: 第一集買了一本，第二集也買了一本，價格應為 100 * 2 * 0.95 = 190

--- day2/test/PotterShoppingCartTest.js
+++ day2/test/PotterShoppingCartTest.js
@@ -6,7 +6,7 @@ import PotterShoppingCart from '../PotterShoppingCart.js';
 describe('Potter Shopping Cart Tests', () => {
   it('只買了第一集，其它都沒買，價何應為 100 * 1 = 100 元', () => {
     // arrange 
-    var books = [new Book('first', 100)];
+    var books = [new Book('第一集', 100)];
 
     let target = new PotterShoppingCart();
     target.setBooks(books);
@@ -20,4 +20,22 @@ describe('Potter Shopping Cart Tests', () => {
     actual.should.equal(expected);
   });
 
+  it('第一集買了一本，第二集也買了一本，價格應為 100 * 2 * 0.95 = 190', () => {
+    // arrange 
+    var books = [
+      new Book('第一集', 100),
+      new Book('第二集', 100)
+    ];
+
+    let target = new PotterShoppingCart();
+    target.setBooks(books);
+
+    let expected = 190;
+
+    //act
+    let actual = target.getPrice();
+
+    //assert
+    actual.should.equal(expected);
+  });
 });
